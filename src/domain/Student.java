0710dbf3cc5e522dfa6711460af8aa7c880fddce
Esunsin <jinchan1148@gmail.com>
2024-01-11 15:59:23 +0900

    add : frame, initData

--- src/domain/Student.java
+++ src/domain/Student.java
@@ -1,8 +1,15 @@
 package domain;
 
 public class Student {
-    private Integer studentId;
+    private Integer studentId =0;
     private String name;
     private SubjectList subjectList;
-    private ScoreList scoreList;
+
+    public Student(String name) {
+        this.studentId++;
+        this.name = name;
+    }
+    public void addSubjectToStudent(Subject subject){
+        this.subjectList.addSubject(subject);
+    }
 }
--- src/domain/SubjectList.java
+++ src/domain/SubjectList.java
@@ -1,7 +1,13 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SubjectList {
-    private List<Subject> subjectList;
+    private List<Subject> subjectList = new ArrayList<>();
+
+    public void addSubject(Subject subject){
+        subjectList.add(subject);
+    }
+
 }
